7409267f1b0e934d2e443cbb731537eb90e305f3
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-03-25 18:00:14 +0100

    Deployed the DAO Smart Contract

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,6 @@
+
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x9b3e3D2eC155145aC2276264E3FbE37A69B594F7";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
